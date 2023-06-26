03b4a4f3a09aa914fec4ed5119c9a596d3644aaa
alexander <alexander_pere@hotmail.com>
2023-06-26 16:53:29 -0500

    cambios

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePluginGhPages } from 'vite-plugin-gh-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()], 
+  plugins: [react(),
+    VitePluginGhPages({
+      branch: 'gh-pages', // Rama en la que se desplegará
+      dir: 'dist', // Carpeta de salida de la compilación
+      push: true, // Realizar push a la rama gh-pages automáticamente
+    }),], 
 })
